ce109aba8da70edd41d2962455302c7d7820038a
freetes <www.1030415864@qq.com>
2018-06-20 13:45:07 +0800

    Beauty change

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+let html = ''
+
+app.use('/public', express.static('public'));
+app.use('/config', express.static('config'));
+app.use('/articles', express.static('articles'));
+
+app.get('/', (req, res) => {
+  // if(html != ''){
+  //   return res.end(html)
+  // }
+  // else{
+    fs.readFile('./index.html', 'utf-8', (err, data)=>{
+      html = data
+      return res.end(html)
+    })
+  // }
+});
+
+const server = app.listen(3000, function () {
+  console.log('app is running at http://localhost:%s', server.address().port);
+});
